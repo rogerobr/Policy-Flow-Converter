ac63c61993be801c42d7d6f64e2c29b49f66c8ec
rogerobr <57853749+rogerobr@users.noreply.github.com>
2020-11-19 00:26:47 -0300

    Update iperf.sh

--- desempenho/iperf.sh
+++ desempenho/iperf.sh
@@ -4,7 +4,7 @@ contador=0
 
 while [ $contador -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 100 >> 100mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 100 >> 100mb.txt
         sleep 1;
         let contador=$contador+1;
 done
@@ -14,7 +14,7 @@ contador2=0
 
 while [ $contador2 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 500 >> 500mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 500 >> 500mb.txt
         sleep 1;
         let contador2=$contador2+1;
 done
@@ -23,7 +23,7 @@ contador3=0
 
 while [ $contador3 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 1000 >> 1000mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 1000 >> 1000mb.txt
         sleep 1;
         let contador3=$contador3+1;
 done
@@ -32,7 +32,7 @@ contador4=0
 
 while [ $contador4 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 1500 >> 1500mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 1500 >> 1500mb.txt
         sleep 1;
         let contador4=$contador4+1;
 done
@@ -41,7 +41,7 @@ contador5=0
 
 while [ $contador5 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 2000 >> 2000mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 2000 >> 2000mb.txt
         sleep 1;
         let contador5=$contador5+1;
 done
@@ -50,7 +50,7 @@ contador6=0
 
 while [ $contador6 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 2500 >> 2500mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 2500 >> 2500mb.txt
         sleep 1;
         let contador6=$contador6+1;
 done
@@ -59,7 +59,7 @@ contador7=0
 
 while [ $contador7 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 3000 >> 3000mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 3000 >> 3000mb.txt
         sleep 1;
         let contador7=$contador7+1;
 done
@@ -68,7 +68,7 @@ contador8=0
 
 while [ $contador8 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 3100 >> 3100mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 3100 >> 3100mb.txt
         sleep 1;
         let contador8=$contador8+1;
 done
@@ -77,7 +77,7 @@ contador9=0
 
 while [ $contador9 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 3200 >> 3200mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 3200 >> 3200mb.txt
         sleep 1;
         let contador9=$contador9+1;
 done
@@ -86,7 +86,7 @@ contador10=0
 
 while [ $contador10 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 6400 >> 6400mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 6400 >> 6400mb.txt
         sleep 1;
         let contador10=$contador10+1;
 done
@@ -95,7 +95,7 @@ contador11=0
 
 while [ $contador11 -lt 30 ];
 do
-        iperf3 -c 10.1.0.10 -u -b 10m -l 12800 >> 12800mb.txt
+        iperf3 -c 10.1.0.10 -u -b 100m -l 12800 >> 12800mb.txt
         sleep 1;
         let contador11=$contador11+1;
 done
